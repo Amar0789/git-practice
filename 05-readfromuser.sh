42a04d7858cb4481125e2f33370ce037ace059ac
Amar <amarsaiyellanki789@gmail.com>
2024-09-14 16:13:21 +0530

    committing arguments

---
+++ 05-readfromuser.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Please enter name of first friend"
+
+read person1
+
+echo "Please enter name of second friend"
+
+read person2
+
+echo "This is a conversation between 2 friends $person1 and $person2"
+echo "$person1:: Hi $person1 how are you? "
+echo "$person2:: Hi $person2 i am good and what about you?"
+echo "$person1::Even i am doing good, It's been a while e haven't met "
+echo "$person2:: Yes its been 2 months we haven't met"
