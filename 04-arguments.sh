b7530cab9b37a0d381133357fe1c920059d5c2c0
Amar <amarsaiyellanki789@gmail.com>
2024-09-14 15:50:30 +0530

    committing arguments

---
+++ 04-arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "This is a conversation between 2 friends"
+echo "$person1:: Hi $person1 how are you? "
+echo "$person2:: Hi $person2 i am good and what about you?"
+echo "$person1::Even i am doing good, It's been a while e haven't met "
+echo "$person2:: Yes its been 2 months we haven't met"
