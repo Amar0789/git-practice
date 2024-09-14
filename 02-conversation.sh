5844d61d4cf52b83ecc114021c1c8576cf608d92
Amar <amarsaiyellanki789@gmail.com>
2024-09-14 15:00:05 +0530

    This is a conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo"This is a conversation between 2 friends"
+echo"Ramesh:: Hi Suresh how are you? "
+echo"Suresh:: Hi Ramesh i am good and what about you?"
+echo"Ramesh::Even i am doing good, It's been a while e haven't met "
+echo"Suresh:: Yes its been 2 months we haven't met"
