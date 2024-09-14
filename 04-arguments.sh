60241320fb39409867fd01feb4008610c5ee6459
Amar <amarsaiyellanki789@gmail.com>
2024-09-14 15:52:58 +0530

    committing arguments

--- 04-arguments.sh
+++ 04-arguments.sh
@@ -2,7 +2,7 @@
 person1=$1
 person2=$2
 
-echo "This is a conversation between friends $person1 and $person2"
+echo "This is a conversation between 2 friends $person1 and $person2"
 echo "$person1:: Hi $person1 how are you? "
 echo "$person2:: Hi $person2 i am good and what about you?"
 echo "$person1::Even i am doing good, It's been a while e haven't met "
