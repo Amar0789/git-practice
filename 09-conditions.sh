57d4648dd098a7ebf6c96591272f346feeb02f12
Amar <amarsaiyellanki789@gmail.com>
2024-09-14 22:20:20 +0530

    committing arguments

---
+++ 09-conditions.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "please enter a number "
+
+read num 
+
+if[$num>20]
+then 
+echo "the number you have entered is greater then 20"
+else 
+echo "the number you have entered is less then 20"
+fi
