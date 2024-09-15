b4ada6bafb0b88587fe8595166ff3816ed8418dc
Amar <amarsaiyellanki789@gmail.com>
2024-09-15 14:31:10 +0530

    This is a commit message

--- 12-forloop.sh
+++ 12-forloop.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-for number in {2..200}
+for packagename in $@
 
 do 
 
- echo "The number is $number"
+ userid=$(id -u)
+
+ if [ $userid -ne 0 ] 
+
+  then 
+
+  echo "please proceed with root privileges"
+
+  exit 
+
+ fi
+
+  dnf list installed $packagename 
+
+ if [ $? -eq 0 ]
+
+  then 
+
+  echo "$pacakagename is already intalled"
+
+ else 
+
+    dnf $packagename git -y
+
+    echo -e "$packagename is \e[33m installed now \e[0m"
+
+ fi
 
 done
