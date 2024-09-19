b60cdb4012d9b1ea1f57ad87e74cf36a9cd7ce1a
Amar <amarsaiyellanki789@gmail.com>
2024-09-19 17:41:00 +0530

    Changes are being committed

--- 11-practice-all.sh
+++ 11-practice-all.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-echo "RESTART"
+friend1 = Amar
+friend2 = Arun
+
+
+echo "Welcome to the conversation between $friend1 and $friend2"
+
+echo "$friend1 - Hey $friend2? Is it you?"
+
+echo "$friend2 - Oh $friend1! How are you? It's been a long time."
+
+echo "$friend1 - I am fine, what about you? Yes, we last met during the board exams."
+
+echo "$friend2 - I'm good too."
+
+echo "$friend1 - What are you doing now?"
+
+echo "$friend2 - Well, I have started my undergraduate studies in English Honours at St. Xaviers College in Mumbai."
+
+echo "$friend1 - Wow! You finally got to study the subject you loved the most in school."
+
+echo "$friend2 - True. What about you $friend1? Wasn't History your favourite subject?"
+
+echo "$friend1 - You guessed it right. I took up History Honours in Lady Shri Ram College for Women in Delhi."
+
+echo "$friend2 - That's nice. I am so happy for you."
+
+echo "$friend1 - I am happy for you too. Let's meet up again soon."
+
+echo "$friend2 - Yes, sure! We have a lot to catch up on."
+
+echo "$friend1 - Bye for now. I have to pick up my sister from tuition. Take care."
+
+echo "$friend2 - Bye, will see you soon."
