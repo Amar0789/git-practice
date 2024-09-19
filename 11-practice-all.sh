4916bf8289c4f065d277c732a6072458190946ac
Amar <amarsaiyellanki789@gmail.com>
2024-09-19 18:16:55 +0530

    Arrays practice

--- 11-practice-all.sh
+++ 11-practice-all.sh
@@ -1,54 +1,25 @@
-#!/bin/bash
+#!/bin/bas
 
+echo "Please enter your favoutite fruit1"
 
-echo "please mention your friend1 name"
-read friend1
+read 1
 
-echo "please mention your friend2 name"
-read friend2
+echo "Please enter your favoutite fruit1"
 
+read 2
 
-echo "Hi $friend1 please enter your password"
-read -s passwd1
+echo "Please enter your favoutite fruit1"
 
-echo "Hi $friend2 please enter your password"
-read -s passwd2
+read 3
 
-echo "Hi $friend1 your password is $passwd1"
+echo "Please enter your favoutite fruit1"
 
-echo "Hi $friend2 your password is $passwd2"
+read 4
 
+echo "Please enter your favoutite fruit1"
 
-#friend1=$1
-#friend2=$2
+read 5
 
+fruits=("$1" "$2" "$3" "$4" "$5")
 
-echo "Welcome to the conversation between $friend1 and $friend2"
-
-echo "$friend1 - Hey $friend2? Is it you?"
-
-echo "$friend2 - Oh $friend1! How are you? It's been a long time."
-
-echo "$friend1 - I am fine, what about you? Yes, we last met during the board exams."
-
-echo "$friend2 - I'm good too."
-
-echo "$friend1 - What are you doing now?"
-
-echo "$friend2 - Well, I have started my undergraduate studies in English Honours at St. Xaviers College in Mumbai."
-
-echo "$friend1 - Wow! You finally got to study the subject you loved the most in school."
-
-echo "$friend2 - True. What about you $friend1? Wasn't History your favourite subject?"
-
-echo "$friend1 - You guessed it right. I took up History Honours in Shri Ram College for men in Delhi."
-
-echo "$friend2 - That's nice. I am so happy for you."
-
-echo "$friend1 - I am happy for you too. Let's meet up again soon."
-
-echo "$friend2 - Yes, sure! We have a lot to catch up on."
-
-echo "$friend1 - Bye for now. I have to pick up my sister from tuition. Take care."
-
-echo "$friend2 - Bye, will see you soon."
+echo "My favourite fruit is ${fruits[0]}" 
