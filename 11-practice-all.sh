5886a57824bbf3457b13acff4dcca4382f68053e
Amar <amarsaiyellanki789@gmail.com>
2024-09-19 18:31:26 +0530

    committing

--- 11-practice-all.sh
+++ 11-practice-all.sh
@@ -1,5 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
-command= ls -la
+echo "please enter num1"
 
-echo "The list of files in this directory are $command"
+read num1
+
+echo "please enter num2"
+
+read num2
+
+num3=$(($num1+$num2))
+
+echo "sum of given 2 numbers are $num3"
+
