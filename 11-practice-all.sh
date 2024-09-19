5562fcb73e61220f86f3f6b3d76d6f2a9682bb19
Amar <amarsaiyellanki789@gmail.com>
2024-09-19 18:24:02 +0530

    committing

--- 11-practice-all.sh
+++ 11-practice-all.sh
@@ -1,26 +1,5 @@
-#!/bin/bas
+#! /bin/bash
 
-echo "Please enter your favoutite fruit1"
+command=ls -la
 
-read one
-
-echo "Please enter your favoutite fruit1"
-
-read two
-
-echo "Please enter your favoutite fruit1"
-
-read three
-
-echo "Please enter your favoutite fruit1"
-
-read four
-
-echo "Please enter your favoutite fruit1"
-
-read five
-
-fruits=("$one" "$two" "$three" "$four" "$five")
-
-
-echo "My favourite fruit is ${fruits[0]}" 
+echo "The list of files in this directory are $command"
