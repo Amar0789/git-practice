c0988aff2200bb30bbff388f2ee36afccb0df50e
Amar <amarsaiyellanki789@gmail.com>
2024-09-19 17:31:24 +0530

    Changes are being committed

--- 11-practice-all.sh
+++ 11-practice-all.sh
@@ -1,29 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 
-userid=$(id -u)
-
-if [ $userid -ne 0 ] 
-
- then 
-
-  echo "please proceed with root privileges"
-
-  exit 
-
-fi
-
-dnf list installed git 
-
-if [ $? -eq 0 ]
-
-then 
-
-echo "git is already intalled"
-
-else 
-
-    dnf install git -y
-
-    echo -e "Git is \e[33m installed now \e[0m"
-
-fi
+echo "RESTART"
