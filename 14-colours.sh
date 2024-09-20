bab0e7521a7c1ff65fe988d4db5dc0f420dd6c7c
Amar <amarsaiyellanki789@gmail.com>
2024-09-20 19:20:30 +0530

    committing my changes

--- 14-colours.sh
+++ 14-colours.sh
@@ -33,13 +33,13 @@ $usercheck
 
 if [ $? -eq 0 ]
 
- then echo "$R $packagename is already installed $N"
+ then echo " $R $packagename is already installed $N "
 
  else 
 
  yum install $packagename -y
 
- echo "$G $packagename is getting installed $N"
+ echo " $G $packagename is getting installed $N "
 
  
 fi
